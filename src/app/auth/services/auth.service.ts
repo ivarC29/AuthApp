ea274f2431f3c997c63c7fbe5dd64f20d88b0127
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-04-05 21:48:27 -0500

    feat(auth service): add check-token  (2 modified, 1 new file) solves the problem of losing authentication when reloading the page

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +21,13 @@ export class AuthService {
 
   constructor() { }
 
+  private setAuthentication(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set( AuthStatus.authenticated );
+    localStorage.setItem('token', token);
+    return true;
+  }
+
   login( email: string, password: string ): Observable<boolean> {
 
     const url = `${ this.baseUrl }/auth/login`;
@@ -28,16 +35,31 @@ export class AuthService {
 
     return this.http.post<LoginResponse>(url, body)
       .pipe(
-        tap( ({ user, token }) => {
-          this._currentUser.set(user);
-          this._authStatus.set( AuthStatus.authenticated );
-          localStorage.setItem('token', token);
-          console.log({ user, token });
-        }),
-        map( () => true ),
+        map( ({ user, token }) => this.setAuthentication( user, token ) ),
         catchError( err =>  throwError( () => err.error.message )
         )
       );
+
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${ this.baseUrl }/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if ( !token ) return of(false);
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<CheckTokenResponse>(url, {headers})
+      .pipe(
+        map(({token, user}) => this.setAuthentication( user, token ) ),
+        catchError(()=> {
+          this._authStatus.set( AuthStatus.notAuthenticated );
+          return of(false)
+        })
+      );
+
 
   }
 
